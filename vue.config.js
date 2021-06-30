c1a7941782e25282fadd90f5724de2db4261e737
Rene Rubalcava <rrubalcava@esri.com>
2021-06-30 10:11:47 -0700

    need resolve for runtime css image refs, remove deployed css

--- vue.config.js
+++ vue.config.js
@@ -9,8 +9,8 @@ module.exports = {
         patterns: [
           // calcite assets
           {
-            context: 'node_modules/@esri/calcite-components/dist/calcite/',
-            from: '**',
+            context: 'node_modules',
+            from: '@esri/calcite-components/dist/calcite',
             to: './',
           },
         // arcgis assets
@@ -20,7 +20,7 @@ module.exports = {
           to: './assets',
           globOptions: {
             // ignore the webscene spec folder, sass files,
-            ignore: ['**/webscene/spec/**', '**/*.scss'],
+            ignore: ['**/webscene/spec/**', '**/*.scss', '**/*.css'],
           },
         },
         ]
@@ -32,6 +32,26 @@ module.exports = {
       config.module
         .rule('scss')
         .oneOf(rule)
+        .use("resolve-url-loader")
+        .loader("resolve-url-loader")
+        .options({
+          sourceMap: true,
+          // eslint-disable-next-line no-unused-vars
+          join: (_rul_uri, _rul_base) => {
+            // args must be included
+            return (arg) => {
+              return arg.bases.value.includes("@arcgis/core")
+                ? arg.uri.replace("../", "./assets/esri/themes/")
+                : arg.uri;
+            };
+          },
+        })
+        .before("sass-loader")
+        .end()
+        .use("sass-loader")
+        .loader("sass-loader")
+        .tap((options) => ({ ...options, sourceMap: true }))
+        .end()
         .use('css-loader')
         .loader('css-loader')
         .tap((options) => ({ ...options, url: false, importLoaders: 2 }))
